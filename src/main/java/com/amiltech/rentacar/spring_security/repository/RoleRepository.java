3d367d4dcac4f8894312c5464d8ce5afdadfaa36
AmilMammadl1 <146669606+AmilMammadl1@users.noreply.github.com>
2025-02-19 12:24:52 +0400

    Update RoleRepository.java

--- src/main/java/com/amiltech/rentacar/spring_security/repository/RoleRepository.java
+++ src/main/java/com/amiltech/rentacar/spring_security/repository/RoleRepository.java
@@ -1,10 +1,11 @@
 package com.amiltech.rentacar.spring_security.repository;
 
+import com.amiltech.rentacar.spring_security.model.Role;
 import com.amiltech.rentacar.spring_security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<User, Long> {
-
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
