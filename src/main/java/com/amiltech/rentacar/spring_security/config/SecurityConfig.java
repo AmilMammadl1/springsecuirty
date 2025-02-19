c56c55f64bf904216f7425fb81f2e9b3540c0922
AmilMammadl1 <146669606+AmilMammadl1@users.noreply.github.com>
2025-02-19 12:24:28 +0400

    Create SecurityConfig.java

---
+++ src/main/java/com/amiltech/rentacar/spring_security/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.amiltech.rentacar.spring_security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationService jwtAuthenticationService;
+
+    @Bean
+    @SuppressWarnings("all")
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .cors();
+        http
+        .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.requestMatchers("/login").permitAll()
+                                .requestMatchers("/register").permitAll()
+                                .requestMatchers("/a").hasRole("ADMIN")
+                                .requestMatchers("/b").hasRole("USER")
+                                .anyRequest().denyAll())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(jwtAuthenticationService, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+}
