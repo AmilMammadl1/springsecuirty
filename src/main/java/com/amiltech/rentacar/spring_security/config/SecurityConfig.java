1f3b63ed4f4eed0c25328c98b3daa63f8a2e6c63
AmilMammadl1 <146669606+AmilMammadl1@users.noreply.github.com>
2025-02-19 12:59:20 +0400

    Update SecurityConfig.java

--- src/main/java/com/amiltech/rentacar/spring_security/config/SecurityConfig.java
+++ src/main/java/com/amiltech/rentacar/spring_security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.amiltech.rentacar.spring_security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,9 +23,10 @@ public class SecurityConfig {
         .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.requestMatchers("/login").permitAll()
                                 .requestMatchers("/register").permitAll()
-                                .requestMatchers("/a").hasRole("ADMIN")
-                                .requestMatchers("/b").hasRole("USER")
-                                .anyRequest().denyAll())
+                                .requestMatchers(HttpMethod.POST,"/a").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET,"/a").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/b").hasRole("USER")
+                                .requestMatchers(HttpMethod.GET,"/b").permitAll()                                .anyRequest().denyAll())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(jwtAuthenticationService, UsernamePasswordAuthenticationFilter.class);
         return http.build();
