99c353b6661fb8a9f285418531af34a4f0a61caa
AmilMammadl1 <146669606+AmilMammadl1@users.noreply.github.com>
2025-02-28 17:29:44 +0400

    Update UserController.java

--- src/main/java/com/amiltech/rentacar/spring_security/controller/UserController.java
+++ src/main/java/com/amiltech/rentacar/spring_security/controller/UserController.java
@@ -7,10 +7,7 @@ import com.amiltech.rentacar.spring_security.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,5 +38,11 @@ public class UserController {
     @PostMapping("/b")
     public ResponseEntity<String> bAuth() {
         return ResponseEntity.status(HttpStatus.OK).body("b");
+    }
+
+    @GetMapping("/verify")
+    public ResponseEntity<Void> verifyUser(@RequestParam String token) {
+        authenticationService.verifyUser(token);
+        return ResponseEntity.ok().build();
     }
 }
